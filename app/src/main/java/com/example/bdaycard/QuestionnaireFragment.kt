e33efc31882e2c3ba6135c277a79a8bf089ab5a4
harikrishnan <2310hari@gmail.com>
2021-11-10 19:18:32 +0530

    Feature: Basic validations for Questionnaire

--- app/src/main/java/com/example/bdaycard/QuestionnaireFragment.kt
+++ app/src/main/java/com/example/bdaycard/QuestionnaireFragment.kt
@@ -1,10 +1,10 @@
 package com.example.bdaycard
 
-import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputLayout
@@ -13,22 +13,33 @@ class QuestionnaireFragment : Fragment(R.layout.fragment_questionnaire) {
 
     override fun onStart() {
         super.onStart()
-        val datePicker =
-            MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select date")
-                .build()
+        val datePicker = getDatePicker()
+        initView(datePicker)
+    }
+
+    private fun initView(datePicker: MaterialDatePicker<Long>) {
         val fab = view?.findViewById<FloatingActionButton>(R.id.fab)
         val dateOfBirth = view?.findViewById<TextInputLayout>(R.id.dob)
-        dateOfBirth?.editText?.setOnClickListener{
+        dateOfBirth?.editText?.setOnClickListener {
             datePicker.show(childFragmentManager, "Tag")
         }
 
-      datePicker.addOnPositiveButtonClickListener { date ->
-          dateOfBirth?.editText?.setText(date.toString())
-          fab?.visibility = View.VISIBLE
-      }
+        datePicker.addOnPositiveButtonClickListener {
+            dateOfBirth?.editText?.setText(datePicker.headerText)
+            fab?.visibility = View.VISIBLE
+        }
         fab?.setOnClickListener {
             findNavController().navigate(R.id.action_questionnaireFragment_to_mainFragment)
         }
+    }
+
+    private fun getDatePicker(): MaterialDatePicker<Long> {
+        val constraintsBuilder =
+            CalendarConstraints.Builder()
+                .setValidator(DateValidatorPointBackward.now())
+        return MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setCalendarConstraints(constraintsBuilder.build())
+            .build()
     }
 }
